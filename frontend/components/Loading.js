0bcb191bc8b9ee3ff7a35cfb3e41f4d8a4aac20b
himshisigmund <himshisigmund@gmail.com>
2025-09-04 23:02:12 -0700

    Update Loading.js

--- frontend/components/Loading.js
+++ frontend/components/Loading.js
@@ -4,11 +4,11 @@ import { ActivityIndicator, View, StyleSheet } from "react-native";
 export default function Loading() {
   return (
     <View style={styles.center}>
-      <ActivityIndicator size="large" color="#007AFF" />
+      <ActivityIndicator size="large" color="#1570ef" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  center: { flex: 1, justifyContent: "center", alignItems: "center" },
+  center: { flex: 1, justifyContent: "center", alignItems: "center", padding: 28 },
 });
